d78a07f46ab7daf92dcb53b24cd1b1743d34c573
Prasoon Pandey <itspandey3131@gmail.com>
2026-06-27 21:27:16 +0530

    Fix useSearchParams Suspense

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,11 +1,10 @@
 "use client";
-
-import { useState, useEffect } from "react";
+import { Suspense, useState, useEffect } from "react";
 import { signIn } from "next-auth/react";
 import { useRouter, useSearchParams } from "next/navigation";
 import Link from "next/link";
 
-export default function LoginPage() {
+function LoginContent()  {
   const router = useRouter();
   const params = useSearchParams();
   const [email, setEmail] = useState("");
@@ -131,5 +130,18 @@ export default function LoginPage() {
         </div>
       </div>
     </div>
+  );
+}
+export default function LoginPage() {
+  return (
+    <Suspense
+      fallback={
+        <div className="min-h-screen flex items-center justify-center">
+          Loading...
+        </div>
+      }
+    >
+      <LoginContent />
+    </Suspense>
   );
 }
